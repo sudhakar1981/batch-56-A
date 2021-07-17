9a78d59611bbed2cf426b83e6da8ec12822a5f3b
sudhakar <sudhakar@gmail.com>
2021-07-17 08:20:03 +0530

    this is second commit for teting

--- payment.java
+++ payment.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 
 /**
  * The main class of the application. It contains the main() method,
- * the first method called.
+ * the first method called, this change for testing
  */
 @NoArgsConstructor
 @AllArgsConstructor
