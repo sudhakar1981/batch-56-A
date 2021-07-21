914712de36220a400206417d586ddaafbd96382b
sudhakar <sudhakar@gmail.com>
2021-07-21 08:05:18 +0530

    Revert "test for revert"
    This reverts commit 5f2fcb31dd3a538c4bd79e7f1f7346b9e38c7046.

--- payment.java
+++ payment.java
@@ -12,8 +12,6 @@ import lombok.Setter;
 /**
  * The main class of the application. It contains the main() method,
  * the first method called, this change for testing
- 
- this change for revert.
  */
 @NoArgsConstructor
 @AllArgsConstructor
